f2feb84dfaccb3b1665e6f86afaf05fefd5c1741
tvaidotas <tadas.vaidotas@gmail.com>
2019-09-26 11:37:43 +0100

    updated import

--- src/test/scala/baseclasses/FunSuiteBaseClass.scala
+++ src/test/scala/baseclasses/FunSuiteBaseClass.scala
@@ -1,7 +1,7 @@
 package baseclasses
 
 import org.scalatest._
-import org.scalatest.mockito.MockitoSugar
+import org.scalatestplus.mockito.MockitoSugar
 
 abstract class FunSuiteBaseClass extends FunSuite with Matchers
   with OptionValues with Inside with Inspectors with MockitoSugar
